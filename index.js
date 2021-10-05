eeb90304aecc84dc8545721caec5238fb61f2e0c
Jesus Israel Garcia <voli.scorp13@gmail.com>
2021-10-05 14:23:49 -0500

    added main routes, css, images

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const { resolve } = require('path');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 8088;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(resolve('public', 'views', 'index.html'))
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile(resolve('public', 'views', 'about.html'))
+})
+
+app.get('/signIn', (req, res) => {
+    res.sendFile(resolve('public', 'views', 'signIn.html'))
+})
+app.get('*', (req, res) => {
+    res.sendFile(resolve('public', 'views', 'index404.html'))
+})
+
+app.listen(port, () => {
+    console.log('Listeninig on port ', port)
+})
