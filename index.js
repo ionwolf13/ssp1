028eac2feef369a95224102ecda480404864dfed
Jesus Israel Garcia <voli.scorp13@gmail.com>
2021-10-05 19:40:32 -0500

    fixed html css template

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
+// app.use('/comp', require('./routes/subs/company.js'))
+
+app.get(['/home','/'], (req, res) => {
     res.sendFile(resolve('public', 'views', 'index.html'))
 })
 
@@ -20,8 +22,14 @@ app.get('/about', (req, res) => {
 app.get('/signIn', (req, res) => {
     res.sendFile(resolve('public', 'views', 'signIn.html'))
 })
+
+app.get('/signUp', (req, res) => {
+    res.sendFile(resolve('public', 'views', 'signUp.html'))
+})
+
 app.get('*', (req, res) => {
     res.sendFile(resolve('public', 'views', 'index404.html'))
+    res.status(404)
 })
 
 app.listen(port, () => {
