45bbadc4bc409b4d284e09e517595fffdfa9105f
Jesus Israel Garcia <voli.scorp13@gmail.com>
2021-10-07 08:06:02 -0500

    fixed design, just need to add more detail, finish logic for user

--- routes/subs/signs.js
+++ routes/subs/signs.js
@@ -7,6 +7,10 @@ router.get('/in', (req, res) => {
     res.sendFile(resolve('public', 'views', 'signIn.html'))
 })
 
+router.post('/in', (req, res) => {
+    console.log(req.body)
+})
+
 router.get('/up', (req, res) => {
     res.sendFile(resolve('public', 'views', 'signUp.html'))
 })
